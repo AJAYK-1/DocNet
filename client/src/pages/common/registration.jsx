5a1c243a4289af14e56a15672f477f353885912e
AJAYK-1 <ajaykumartp10@gmail.com>
2026-02-26 22:20:33 +0530

    "api change"

--- client/src/pages/common/registration.jsx
+++ client/src/pages/common/registration.jsx
@@ -12,19 +12,25 @@ export default function CombinedRegistration() {
     name: '',
     email: '',
     password: '',
-    role: 'patient'
+    role: 'patient',
   });
 
-  const [doctorForm, setDoctorForm] = useState({
-    name: '',
-    email: '',
-    password: '',
-    role: 'doctor',
-    address: '',
-    license: '',
-    qualification: '',
-    specialization: ''
-  });
+  const [tab, setTab] = useState('patient')
+
+  const handleTabChange = key => {
+    setTab(key)
+    if (key === 'patient')
+      setUserForm({ ...userForm, role: key })
+    else
+      setUserForm({
+        ...userForm,
+        role: key,
+        address: '',
+        license: '',
+        qualification: '',
+        specialization: ''
+      })
+  }
 
   const [doctorImage, setDoctorImage] = useState(null);
   const navigate = useNavigate();
@@ -35,23 +41,19 @@ export default function CombinedRegistration() {
 
   const handleUserSubmit = async (e) => {
     e.preventDefault();
-    AXIOS.post(`${import.meta.env.VITE_HOST_URL}/api/user/registration`, userForm)
-      .then((res) => {
-        if (res.status == 200) {
-          toast.success(res.data.msg);
-          setUserForm({ name: '', email: '', password: '' });
-          setTimeout(() => navigate('/login'), 2000);
-        } else if (res.status == 400) {
-          toast.error(res.data.msg)
-        }
-      }).catch((err) => {
-        console.log(err);
-        toast.error("Something went wrong...");
-      });
-  };
-
-  const handleDoctorChange = (e) => {
-    setDoctorForm({ ...doctorForm, [e.target.name]: e.target.value });
+    try {
+      const response = await AXIOS.post(`${import.meta.env.VITE_HOST_URL}/api/user/registration`, userForm)
+      if (response.status === 200) {
+        toast.success(response.data.msg);
+        setUserForm({ name: '', email: '', password: '' });
+        setTimeout(() => navigate('/login'), 2000);
+      } else {
+        toast.error(response.data.msg)
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error("Something went wrong...");
+    }
   };
 
   const handleImageChange = (e) => {
@@ -60,34 +62,33 @@ export default function CombinedRegistration() {
 
   const handleDoctorSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('name', doctorForm.name)
-    formData.append('email', doctorForm.email)
-    formData.append('password', doctorForm.password)
-    formData.append('address', doctorForm.address)
-    formData.append('license', doctorForm.license)
-    formData.append('qualification', doctorForm.qualification)
-    formData.append('specialization', doctorForm.specialization)
-    formData.append('profileImage', doctorImage)
+    try {
+      const formData = new FormData();
+      formData.append('name', userForm.name)
+      formData.append('email', userForm.email)
+      formData.append('password', userForm.password)
+      formData.append('address', userForm.address)
+      formData.append('license', userForm.license)
+      formData.append('qualification', userForm.qualification)
+      formData.append('specialization', userForm.specialization)
+      formData.append('profileImage', doctorImage)
 
-    AXIOS.post(`${import.meta.env.VITE_HOST_URL}/api/user/registration`, formData, {
-      headers: { "Content-Type": "multipart/form-data" }
-    })
-      .then((res) => {
-        if (res.status == 200) {
-          toast.success(res.data.msg);
-          setDoctorForm({ name: '', email: '', password: '', address: '', license: '', qualification: '', specialization: '' });
-          setDoctorImage(null);
-          setTimeout(() => {
-            navigate('/login');
-          }, 2000);
-        } else if (res.status == 400) {
-          toast.error(res.data.msg)
-        }
-      }).catch((err) => {
-        console.log(err);
-        toast.error("Something went wrong...");
-      });
+      const res = await AXIOS.post(`${import.meta.env.VITE_HOST_URL}/api/user/registration`,
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } })
+
+      if (res.status == 200) {
+        toast.success(res.data.msg);
+        setUserForm({ name: '', email: '', password: '', address: '', license: '', qualification: '', specialization: '' });
+        setDoctorImage(null);
+        setTimeout(() => { navigate('/login'); }, 2000);
+      } else {
+        toast.error(res.data.msg)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong...");
+    }
   };
 
   return (
@@ -102,8 +103,8 @@ export default function CombinedRegistration() {
             Register
           </h2>
 
-          <Tabs defaultActiveKey="user" id="registration-tabs" className="mb-4 tabs-nav"  justify>
-            <Tab eventKey="user" title="User">
+          <Tabs defaultActiveKey={tab} activeKey={tab} onSelect={handleTabChange} id="registration-tabs" className="mb-4 tabs-nav" justify>
+            <Tab eventKey="patient" title="User">
               <Form onSubmit={handleUserSubmit}>
                 <FloatingLabel controlId="floatingUserName" label="Your Name" className="mb-3">
                   <Form.Control type="text" name="name" value={userForm.name} onChange={handleUserChange} placeholder="Enter your name" required />
@@ -127,38 +128,38 @@ export default function CombinedRegistration() {
             <Tab eventKey="doctor" title="Doctor">
               <Form onSubmit={handleDoctorSubmit}>
                 <FloatingLabel controlId="floatingDocName" label="Doctor Name" className="mb-3">
-                  <Form.Control type="text" name="name" onChange={handleDoctorChange} placeholder="Enter name" required />
+                  <Form.Control type="text" name="name" onChange={handleUserChange} placeholder="Enter name" required />
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingDocEmail" label="Email address" className="mb-3">
-                  <Form.Control type="email" name="email" onChange={handleDoctorChange} placeholder="Enter email" required />
+                  <Form.Control type="email" name="email" onChange={handleUserChange} placeholder="Enter email" required />
                   <Form.Text className="text-muted">We'll never share your email.</Form.Text>
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingDocPassword" label="Password" className="mb-3">
-                  <Form.Control type="password" name="password" onChange={handleDoctorChange} placeholder="Password" required />
+                  <Form.Control type="password" name="password" onChange={handleUserChange} placeholder="Password" required />
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingLicense" label="Medical License Number" className="mb-3">
-                  <Form.Control type="text" name="license" onChange={handleDoctorChange} placeholder="Enter License Number" required />
+                  <Form.Control type="text" name="license" onChange={handleUserChange} placeholder="Enter License Number" required />
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingQualification" label="Qualification" className="mb-3">
-                  <Form.Control type="text" name="qualification" onChange={handleDoctorChange} placeholder="Qualification" required />
+                  <Form.Control type="text" name="qualification" onChange={handleUserChange} placeholder="Qualification" required />
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingSpecialization" label="Specialization" className="mb-3">
-                  <Form.Select name="specialization" onChange={handleDoctorChange} required>
+                  <Form.Select name="specialization" onChange={handleUserChange} required>
                     <option value="">-- Select Specialization --</option>
                     {SPECIALIZATIONS.map((specialization) =>
                       <option value={specialization}>{specialization}</option>
                     )}
-                    
+
                   </Form.Select>
                 </FloatingLabel>
 
                 <FloatingLabel controlId="floatingDocAddress" label="Address" className="mb-3">
-                  <Form.Control as="textarea" name="address" onChange={handleDoctorChange} style={{ height: '100px' }} placeholder="Enter address" required />
+                  <Form.Control as="textarea" name="address" onChange={handleUserChange} style={{ height: '100px' }} placeholder="Enter address" required />
                 </FloatingLabel>
 
                 <Form.Group className="mb-3">
